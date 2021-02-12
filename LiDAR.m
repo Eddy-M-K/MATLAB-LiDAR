10fe4a03bea2aa1e6176f14ada0cc1268024419a
Edward Kim <Eddy.M.K@outlook.com>
2021-02-12 10:27:32 -0500

    Minor edits

--- LiDAR.m
+++ LiDAR.m
@@ -13,9 +13,9 @@ RPM = 1200;
 r = 0.021335; 
 
 % LiDAR Sensor Location Shift
-LiDAR_x = 10;
-LiDAR_y = -1;
-LiDAR_z = 1;
+LiDAR_x = 6;
+LiDAR_y = -2.3;
+LiDAR_z = 0.7;
 
 % Azimuth Resolution Calculation for Non Accurate Calculations where 16
 % Lasers are assumed to fire at once
@@ -53,54 +53,54 @@ Precise_Azimuth = 0; % RPM / 60 * 360 * 2.304*10^-6 * 16 * (Enter Whole number h
 
 % -------------------------------------------------------------------------
 
-FOV_Start = 270;
-FOV_End = 450;
-Lower_Angle = -15;
-Upper_Angle = 15;
-
-sphere_moving_position1 = readmatrix('LiDAR.xlsx');
-sphere_moving_x1 = sphere_moving_position1(:,1);
-sphere_moving_y1 = sphere_moving_position1(:,3);
-sphere_moving_z1 = sphere_moving_position1(:,2);   
-
-LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, 10, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
-Golf_Ball_Trajectory(sphere_moving_x1, sphere_moving_y1, sphere_moving_z1, r)    
-
-sphere_moving_position2 = readmatrix('LiDAR_Precise.xlsx');
-sphere_moving_x2 = sphere_moving_position2(:,1);
-sphere_moving_y2 = sphere_moving_position2(:,3);
-sphere_moving_z2 = sphere_moving_position2(:,2);
-
-hold off
-figure
-hold on
-
-Azimuth = 0;
-Moving_Golf_Ball_Intersection(Azimuth, sphere_moving_x2, sphere_moving_y2, sphere_moving_z2, r, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
-
-% -------------------------------------------------------------------------
-
 % FOV_Start = 270;
 % FOV_End = 450;
 % Lower_Angle = -15;
 % Upper_Angle = 15;
 % 
-% sphere_moving_position3 = readmatrix('LiDAR2.xlsx');
-% sphere_moving_x3 = sphere_moving_position3(:,1);
-% sphere_moving_y3 = sphere_moving_position3(:,3);
-% sphere_moving_z3 = sphere_moving_position3(:,2);   
+% sphere_moving_position1 = readmatrix('LiDAR.xlsx');
+% sphere_moving_x1 = sphere_moving_position1(:,1);
+% sphere_moving_y1 = sphere_moving_position1(:,3);
+% sphere_moving_z1 = sphere_moving_position1(:,2);   
 % 
 % LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, 10, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
-% Golf_Ball_Trajectory(sphere_moving_x3, sphere_moving_y3, sphere_moving_z3, r)    
+% Golf_Ball_Trajectory(sphere_moving_x1, sphere_moving_y1, sphere_moving_z1, r)    
 % 
-% sphere_moving_position4 = readmatrix('LiDAR2_Precise.xlsx');
-% sphere_moving_x4 = sphere_moving_position4(:,1);
-% sphere_moving_y4 = sphere_moving_position4(:,3);
-% sphere_moving_z4 = sphere_moving_position4(:,2);
+% sphere_moving_position2 = readmatrix('LiDAR_Precise.xlsx');
+% sphere_moving_x2 = sphere_moving_position2(:,1);
+% sphere_moving_y2 = sphere_moving_position2(:,3);
+% sphere_moving_z2 = sphere_moving_position2(:,2);
 % 
 % hold off
 % figure
 % hold on
 % 
 % Azimuth = 0;
-% Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x4, sphere_moving_y4, sphere_moving_z4, r, Precise_Azimuth_Resolution, Radius_Start, 10, LiDAR_x, LiDAR_y, LiDAR_z)
+% Moving_Golf_Ball_Intersection(Azimuth, sphere_moving_x2, sphere_moving_y2, sphere_moving_z2, r, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
+
+% -------------------------------------------------------------------------
+
+FOV_Start = 270;
+FOV_End = 450;
+Lower_Angle = -15;
+Upper_Angle = 15;
+
+sphere_moving_position3 = readmatrix('LiDAR2.xlsx');
+sphere_moving_x3 = sphere_moving_position3(:,1);
+sphere_moving_y3 = sphere_moving_position3(:,3);
+sphere_moving_z3 = sphere_moving_position3(:,2);   
+
+LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, 10, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
+Golf_Ball_Trajectory(sphere_moving_x3, sphere_moving_y3, sphere_moving_z3, r)    
+
+sphere_moving_position4 = readmatrix('LiDAR2_Precise.xlsx');
+sphere_moving_x4 = sphere_moving_position4(:,1);
+sphere_moving_y4 = sphere_moving_position4(:,3);
+sphere_moving_z4 = sphere_moving_position4(:,2);
+
+hold off
+figure
+hold on
+
+Azimuth = 0;
+Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x4, sphere_moving_y4, sphere_moving_z4, r, Precise_Azimuth_Resolution, Radius_Start, 10, LiDAR_x, LiDAR_y, LiDAR_z)
