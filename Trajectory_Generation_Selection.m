478ff47cadfd3b26571100ba9bb402d79932b080
Aryan Modgil <a3modgil@uwaterloo.ca>
2021-02-16 13:37:44 -0500

    Update Trajectory_Generation_Selection.m

--- Trajectory_Generation_Selection.m
+++ Trajectory_Generation_Selection.m
@@ -1,10 +1,10 @@
 function [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Generation_Selection(dt)
     Selection = input("Lift, Drag, Both, or just Gravity: ", 's');
-    if strcmpi(Selection, 'Drag')
+    if strcmpi(Selection, 'Drag-H')
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Drag(v0, theta, dt);
-    elseif strcmpi(Selection, 'Lift')
+    elseif strcmpi(Selection, 'Lift-H')
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
         wx = input("Rifle spin in rads/sec: ");
@@ -12,7 +12,7 @@ function [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Genera
         wz = input("Back spin in rads/sec: ");
         phi = input("Spin angle in degrees: ");
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Lift(v0, theta, wx, wy, wz, phi, dt);
-    elseif strcmpi(Selection, 'Both')
+    elseif strcmpi(Selection, 'Both-H')
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
         wx = input("Rifle spin in rads/sec: ");
@@ -20,7 +20,7 @@ function [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Genera
         wz = input("Back spin in rads/sec: ");
         phi = input("Spin angle in degrees: ");
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Both(v0, theta, wx, wy, wz, phi, dt);
-    elseif strcmpi(Selection, 'Gravity')
+    elseif strcmpi(Selection, 'Gravity-H')
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Gravity(v0, theta, dt);
