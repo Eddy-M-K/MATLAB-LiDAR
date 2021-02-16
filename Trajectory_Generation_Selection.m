ed1843d509e66d3fd6b56e2529f14116b1ea118a
Edward Kim <Eddy.M.K@outlook.com>
2021-02-16 13:47:45 -0500

    bruh

--- Trajectory_Generation_Selection.m
+++ Trajectory_Generation_Selection.m
@@ -6,7 +6,6 @@ function [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Genera
 
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Drag_H(v0, theta, dt);
     elseif strcmpi(Selection, 'Lift')
-
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
         wx = input("Rifle spin in rads/sec: ");
@@ -14,10 +13,8 @@ function [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Genera
         wz = input("Back spin in rads/sec: ");
         phi = input("Spin angle in degrees: ");
 
-
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Lift_H(v0, theta, wx, wy, wz, phi, dt);
     elseif strcmpi(Selection, 'Both')
-
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
         wx = input("Rifle spin in rads/sec: ");
@@ -29,6 +26,7 @@ function [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Genera
     elseif strcmpi(Selection, 'Gravity')
         v0 = input("Inital velocity in m/s: ");
         theta = input("Angle of the velocity in degrees: ");
+        
         [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Gravity_H(v0, theta, dt);
     end 
     fprintf("\n")
