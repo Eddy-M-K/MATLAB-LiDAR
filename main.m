0d0b38516c8462bf9cdf37ea2ef5323e2584ca11
Eddy-M-K <Eddy.M.K@outlook.com>
2021-04-12 11:14:24 -0400

    Renamed files and added additional comments to main.m

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 % IMPORTANT: This is a template main.m file which calls other functions
-% that don't have the _H tag (files with _H tags are helper functions).
-% Create a copy of this file called "Copy_of_main" for your local system 
-% if you plan to commit changes to the repository. 
+% that don't have the _H tag (files with _H tags are helper functions). 
+
+% Files that are named with "deprecated" are old simulations that are
+% relatively inaccurate compared to the newer intersection finders that are
+% named "Precise Moving Golf Ball Intersection"
 
 % All values are in meters.
 
@@ -10,22 +12,22 @@ close all
 clearvars
 format long
 
-% Variables that should remain constant for accuracy:
 Radius_Start = 0.04191; % Starting length of laser beam
 r = 0.021335;           % Golf ball radius
 
+% Baseball radius is 0.0375;
+% Tennis Ball radius is 0.03429;
+
 % IMPORTANT: If changing to a different ball, ensure you change the mass 
 % and diameter values within Both_H.m or the relevant trajectory generation 
-% function you will be calling:
-
-% Baseball radius 0.0375;
-% Tennis Ball radius 0.03429;
+% option you will be choosing (Both_H., Drag_H.m, Lift_H.m, Gravity_H.m)
 
 % RPM must be in increments of 60 from 300 to 1200
 RPM = 1200; 
 
 % -------------------------------------------------------------------------
-% Laser Intersection with Triangle(s) (Inaccurate)
+
+% Laser Intersection with Triangle(s) (Deprecated)
 %
 % Triangle vertices:
 % P1 = [1 0 2];
@@ -43,9 +45,10 @@ RPM = 1200;
 % 
 % Function Call(s):
 % Triangle_Intersection(P1, P2, P3, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
-%
+
 % -------------------------------------------------------------------------
-% Laser Intersection with Static Golf Ball(s) (Inaccurate)
+
+% Laser Intersection with Static Golf Ball(s) (Deprecated)
 %
 % Shift golf ball's center:
 % sphere_shift_x = 2;
@@ -63,8 +66,9 @@ RPM = 1200;
 %
 % Function Call(s):
 % Static_Golf_Ball_Intersection(sphere_shift_x, sphere_shift_y, sphere_shift_z, r, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
-% 
+
 % -------------------------------------------------------------------------
+
 % General Laser Emission Pattern Generation of a VLP-16 for Visualization
 % Purposes (Inaccurate)
 %
@@ -75,29 +79,32 @@ RPM = 1200;
 %
 % Function Call(s):
 % LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, 10, Radius_Start, Radius_End, LiDAR_x, LiDAR_y, LiDAR_z)
-%
+
 % -------------------------------------------------------------------------
+
 % Example code to generate golf ball trajectory and implement multiple, accurate
 % LiDAR simulations to detect intersections
 
-dt = 2.304*10^-6;
-Precise_Azimuth_Resolution = RPM / 60 * 360 * 2.304*10^-6;
+dt = 2.304*10^-6;                                               % Should remain constant
+Precise_Azimuth_Resolution = RPM / 60 * 360 * 2.304*10^-6;      % Should remain constant
 
-% Change LiDAR's start Azimuth position for spinning (Default is 0):
+% Change LiDAR's start Azimuth position (Default is 0):
 Precise_Azimuth = 0; % RPM / 60 * 360 * 2.304*10^-6 * 16 * (Enter Whole number here)
 
-Radius_End = 10; % (Or some number)
+% Radius of LiDAR sensor lasers 
+Radius_End = 10; 
 
+%Should remain constant
 FOV_Start = 0;
 FOV_End = 359;
 Lower_Angle = -15;
 Upper_Angle = 15;
 Azimuth_Resolution = 30;
 
-% LiDAR positions in order:
+% LiDAR positions in order (Currently there are 4 but more can be added):
 
-LiDAR_x1 = 7;
-LiDAR_y1 = -1;
+LiDAR_x1 = 5;
+LiDAR_y1 = 0;
 LiDAR_z1 = 1;
 
 LiDAR_x2 = 40;
@@ -112,12 +119,21 @@ LiDAR_x4 = 95;
 LiDAR_y4 = -10;
 LiDAR_z4= 0;
 
+% FROM HERE: call the LiDAR emission pattern functions according to where
+% they should be - note that there are perpendicular and parallel
+% configurations as well as the standard upright position:
+
 hold on 
 LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x1, LiDAR_y1, LiDAR_z1)
 Perpendicular_Rotated_LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x2, LiDAR_y2, LiDAR_z2)
 Perpendicular_Rotated_LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x3, LiDAR_y3, LiDAR_z3)
 LiDAR_Emission_Pattern(FOV_Start, FOV_End, Lower_Angle, Upper_Angle, Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x4, LiDAR_y4, LiDAR_z4)
 
+% -------------------------------------------------------------------------
+
+% The below function prompts the user for the conditions for the golf
+% ball's trajectory, generates the golf ball's position, plots it, and
+% passes it onto the Precise Moving Golf Ball Intersection functions below
 [sphere_moving_x, sphere_moving_y, sphere_moving_z] = Trajectory_Generation_Selection(dt);
 
 hold off
@@ -126,10 +142,21 @@ hold on
 
 Export = {'Point of Intersection (x)', 'Point of Intersection (y)', 'Point of Intersection (z)', 'Azimuth', 'Vertical Angle', 'Sphere Center (x)', 'Sphere Center (y)', 'Sphere Center (z)'};
 
+% FROM HERE: call the functions "Precise Moving Golf Ball Intersection"
+% according to the configuration needed. The function calls should match in
+% type and count of the functions called for the emission pattern above
+
 Export = Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x, sphere_moving_y, sphere_moving_z, r, Precise_Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x1, LiDAR_y1, LiDAR_z1, Export);
-Export = Perpendicular_Rotated_Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x, sphere_moving_y, sphere_moving_z, r, Precise_Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x2, LiDAR_y2, LiDAR_z2, Export);
-Export = Perpendicular_Rotated_Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x, sphere_moving_y, sphere_moving_z, r, Precise_Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x3, LiDAR_y3, LiDAR_z3, Export);
+Export = Parallel_Rotated_Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x, sphere_moving_y, sphere_moving_z, r, Precise_Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x2, LiDAR_y2, LiDAR_z2, Export);
+Export = Parallel_Rotated_Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x, sphere_moving_y, sphere_moving_z, r, Precise_Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x3, LiDAR_y3, LiDAR_z3, Export);
 Export = Precise_Moving_Golf_Ball_Intersection(Precise_Azimuth, sphere_moving_x, sphere_moving_y, sphere_moving_z, r, Precise_Azimuth_Resolution, Radius_Start, Radius_End, LiDAR_x4, LiDAR_y4, LiDAR_z4, Export);
+
+% -------------------------------------------------------------------------
 
 filename = 'Exported Points of Intersections (MATLAB LiDAR Simulation).xlsx';
 writecell(Export, filename);
+
+% What results is a figure displaying the general golf ball trajectory and
+% the general LiDAR emission patterns, and another figure with only the
+% intersection lasers. Data is outputted in both the console and in an
+% Excel file
